14cb607812f6750f1d4a46d88d191d30bc31fc18
NayeonPark <uoahvu@gmail.com>
2024-11-25 16:29:41 +0900

    ✨ Add Content Embedding & Searching Module

---
+++ candidate_generation.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+from sentence_transformers import SentenceTransformer
+import faiss
+
+
+class ContentEmbedding(nn.Module):
+    def __init__(self, hidden_size, num_season):
+        super(ContentEmbedding, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_season = num_season
+
+        self.season_embedding = nn.Embedding(num_season, hidden_size)
+        self.description_embedding = SentenceTransformer(
+            "sentence-transformers/all-MiniLM-L6-v2"
+        )
+
+    def forward(self, x):
+        # 시즌
+        embedded_season = self.season_embedding(torch.tensor(x.iloc[:, -2]).long())
+        # 특집
+        special_epicode = nn.functional.one_hot(torch.tensor(x.iloc[:, -1]))
+        # 제목텍스트
+        embedded_title = torch.tensor(self.description_embedding.encode(x.iloc[:, 2]))
+        # 설명텍스트
+        embedded_description = torch.tensor(
+            self.description_embedding.encode(x.iloc[:, 6])
+        )
+        result = torch.concat(
+            [embedded_season, special_epicode, embedded_title, embedded_description],
+            dim=-1,
+        )
+        print(result.shape)
+        return result
+
+    def inference(self, x):
+        output = self(x)
+        index = faiss.IndexFlatL2(output.size(-1))
+        index.add(output.detach().numpy())
+        return index, output
+
+
+def candidate_generator(index, query, k):
+    D, I = index.search(query.reshape(1, -1), k=k)
+
+    return D, I
---
+++ main.py
@@ -0,0 +1,28 @@
+from data_loader import data_preprocess
+from candidate_generation import ContentEmbedding, candidate_generator
+
+if __name__ == "__main__":
+    data = data_preprocess()
+
+    print("CONTENT EMBEDDING ...")
+    hidden_size = 64
+    num_season = data["season"].nunique()
+    embedding_model = ContentEmbedding(hidden_size, num_season)
+    index, content_embedding = embedding_model.inference(data)
+    print("content_embedding", content_embedding)
+
+    print("SEARCH SIMILAR CONTENTS ...")
+    k = 5
+    query_idx = 4
+    query_emb = index.reconstruct(query_idx)
+    _, similar_contents_idx = candidate_generator(index, query_emb, k)
+
+    # print(data.loc[query_idx])
+    # print(data.loc[similar_contents_idx.flatten()])
+
+    print("PAIR-WISE PREFERENCE SURVEY ...")
+    # TODO
+    print("FINE TUNING ...")
+    # TODO
+    print("RECOMMENDATION ...")
+    # TODO
